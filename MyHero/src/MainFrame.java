0dc790f2a0fb4570fe208c922f3ac411d372ed8f
Mindo01 <y229600@naver.com>
2016-05-16 20:09:01 +0900

    minju~

---
+++ MyHero/src/MainFrame.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+// ╧наж╫Х╢ы ©ю╧ы
+public class MainFrame extends JFrame{
+
+	public MainFrame() {
+		setSize(300, 300);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+
+}
